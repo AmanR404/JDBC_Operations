1d7eac7d50438ed91f4330978819da763524438d
Aman <raiaman21@gmail.com>
2022-08-16 21:36:31 +0530

    UC3 Solved

--- JDBCPreparedStatement.java
+++ JDBCPreparedStatement.java
@@ -1,9 +1,12 @@
 import java.sql.*;
-
 class MyDatabaseException extends Exception{
     @Override
     public String getMessage() {
         return "Database not found";
+    }
+    @Override
+    public String toString() {
+        return "Error...Unable to update";
     }
 }
 public class JDBCPreparedStatement {
@@ -23,9 +26,15 @@ public class JDBCPreparedStatement {
             e.printStackTrace();
         }
         try{
-            PreparedStatement preparedstmt = connection.prepareStatement("select * from employee_payroll");
-            ResultSet resultSet = preparedstmt.executeQuery();
-
+            PreparedStatement preparedStmt = connection.prepareStatement("update employee_payroll set salary = '300000' where name = 'Govind'");
+            preparedStmt.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+        try{
+            PreparedStatement preparedStmt2 = connection.prepareStatement("select * from employee_payroll");
+            ResultSet resultSet = preparedStmt2.executeQuery();
             while(resultSet.next()){
                 System.out.println(resultSet.getString("name")+ " " + resultSet.getString("Salary") + " " + resultSet.getString("start_date"));
             }
