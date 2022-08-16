7e0818be0c007336daee93ccf612720efcfd146f
Aman <raiaman21@gmail.com>
2022-08-16 21:09:36 +0530

    UC2 Solved

--- JDBCPreparedStatement.java
+++ JDBCPreparedStatement.java
@@ -1,4 +1,11 @@
 import java.sql.*;
+
+class MyDatabaseException extends Exception{
+    @Override
+    public String getMessage() {
+        return "Database not found";
+    }
+}
 public class JDBCPreparedStatement {
     static Connection connection;
     public static void main(String[] args) {
@@ -10,9 +17,21 @@ public class JDBCPreparedStatement {
             System.out.println("Connecting to Database : " + jdbcURL);
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection is Successful : " + connection);
+            System.out.println(" ");
         }
         catch(Exception e){
             e.printStackTrace();
+        }
+        try{
+            PreparedStatement preparedstmt = connection.prepareStatement("select * from employee_payroll");
+            ResultSet resultSet = preparedstmt.executeQuery();
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("name")+ " " + resultSet.getString("Salary") + " " + resultSet.getString("start_date"));
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
