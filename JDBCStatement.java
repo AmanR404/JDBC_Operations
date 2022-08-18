8ef460501afff3d2614a3bcd0c909bd55617d0eb
Aman <raiaman21@gmail.com>
2022-08-18 13:47:33 +0530

    UC12 Solved

--- JDBCStatement.java
+++ JDBCStatement.java
@@ -1,6 +1,4 @@
 import java.sql.*;
-import org.junit.Assert;
-import org.junit.Test;
 class MyDatabaseException extends Exception{
     @Override
     public String getMessage() {
@@ -14,10 +12,6 @@ class MyDatabaseException extends Exception{
 public class JDBCStatement {
     static Connection connection;
     static ResultSet resultSet;
-    static String idarr[] = new String[3];
-    static String namearr[] = new String[3];
-    static String salaryarr[] = new String[3];
-    @Test
     public static void main(String[] args) throws SQLException{
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
         String userName = "root";
@@ -33,36 +27,25 @@ public class JDBCStatement {
             e.printStackTrace();
         }
         try{
-            connection.setAutoCommit(false);
-            PreparedStatement preparedstmt1 = connection.prepareStatement("insert into employee_payroll (id,name,salary) values (3,'Ameen',40000)");
+            PreparedStatement preparedstmt1 = connection.prepareStatement("alter table employee_payroll add is_active BOOLEAN after start_date");
             preparedstmt1.executeUpdate();
-            connection.commit();
+            connection.prepareStatement("update * from employee_payroll set is_active = true").executeUpdate();
         }
         catch(Exception e){
             System.out.println(e.toString());
         }
 
         try{
-            PreparedStatement preparedStmt2 = connection.prepareStatement("select * from employee_payroll");
-            resultSet = preparedStmt2.executeQuery();
-            int i = 0;
+            PreparedStatement preparedStmt2 = connection.prepareStatement("update employee_payroll set is_active = false where name = 'Ameen'");
+            preparedStmt2.executeUpdate();
+            ResultSet resultSet =  connection.prepareStatement("select * from employee_payroll where is_active = true").executeQuery();
             while(resultSet.next()){
-                idarr[i] = resultSet.getString("id");
-                namearr[i] = resultSet.getString("name");
-                salaryarr[i] = resultSet.getString("salary"); 
-                i++;
+              System.out.println(resultSet.getString("id")+ " " + resultSet.getString("name") + " " + resultSet.getString("salary"));
             }
         }
         catch(SQLException e){
             System.out.println(e.getMessage());
-            connection.rollback();
         }
-        for(int i = 0;i < idarr.length;i++){
-            System.out.print(idarr[i] + " ");
-            System.out.print(namearr[i] + " ");
-            System.out.print(salaryarr[i] + " \n");
-        }
-        Assert.assertEquals("Aman", namearr[0]);
         }
 }
 
